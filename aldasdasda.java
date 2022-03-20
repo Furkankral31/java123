fdbce3fa0c528ec4a13e4ce302fda08d2bf9e89b
Furkandawizard <92584482+Furkankral31@users.noreply.github.com>
2022-03-20 18:18:39 +0300

    Add files via upload

---
+++ aldasdasda.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class aldasdasda {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        float kg;
+        float boy;
+        float endeks;
+        kg=input.nextInt();
+        boy=input.next.Int();
+        endeks=kg/(boy*boy);
+    }
+    }
+
